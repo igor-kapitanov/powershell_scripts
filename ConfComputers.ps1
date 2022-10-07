565a9daee39cd8eadd0df5081fe6fcb68f3e80ad
Igor Kapitanov <70857416+igor-kapitanov@users.noreply.github.com>
2022-10-07 09:05:03 -0400

    Update ConfComputers.ps1

--- ConfComputers.ps1
+++ ConfComputers.ps1
@@ -315,18 +315,18 @@ function CreateUsers
 {
     Write-Host "***** Create admin users *****" -ForegroundColor Green
 	#Create CITadmin user
-	$user1 = "CITadmin"
-	$fname1 = "CITadmin"
-    $password1 = ConvertTo-SecureString "Pft,bcm2" -AsPlainText -Force
-    New-LocalUser $user1 -Password $password1 -FullName $fname1 -Description "CIT admin user"
+	$user1 = "type the name"
+	$fname1 = "type the full name"
+    $password1 = ConvertTo-SecureString "type the password" -AsPlainText -Force
+    New-LocalUser $user1 -Password $password1 -FullName $fname1 -Description "first admin user"
     Set-LocalUser $user1 -AccountNeverExpires -PasswordNeverExpires $true -UserMayChangePassword $false
     Add-LocalGroupMember -Group "Administrators" -Member $user1 -ErrorAction stop
 	
     #Create Install user
-	$user2 = "Install"
-	$fname2 = "Install"
-    $password2 = ConvertTo-SecureString "CloudIT1!" -AsPlainText -Force
-    New-LocalUser $user2 -Password $password2 -FullName $fname2 -Description "second CIT admin user"
+	$user2 = "type the name"
+	$fname2 = "type the full name"
+    $password2 = ConvertTo-SecureString "type the password" -AsPlainText -Force
+    New-LocalUser $user2 -Password $password2 -FullName $fname2 -Description "the second admin user"
     Set-LocalUser $user2 -AccountNeverExpires -PasswordNeverExpires $true -UserMayChangePassword $false
     Add-LocalGroupMember -Group "Administrators" -Member $user2 -ErrorAction stop
 }
