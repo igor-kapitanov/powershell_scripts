79914f2f6868eb3e04a28dfc36a57c5264e8000c
Igor Kapitanov <70857416+igor-kapitanov@users.noreply.github.com>
2022-12-30 12:32:59 -0500

    edited

--- UnivSetup.ps1
+++ UnivSetup.ps1
@@ -254,16 +254,16 @@ function CreateUsers
 {
 	Write-Host "***** Create admin users *****" -ForegroundColor Green
 	#Create CITadmin user
-	$user1 = "CITadmin"
-	$fname1 = "CITadmin"
-    $password1 = ConvertTo-SecureString "Pft,bcm2" -AsPlainText -Force
+	$user1 = "name"
+	$fname1 = "fname"
+    $password1 = ConvertTo-SecureString "passwword" -AsPlainText -Force
     New-LocalUser $user1 -Password $password1 -FullName $fname1 -Description "first admin user"
     Set-LocalUser $user1 -AccountNeverExpires -PasswordNeverExpires $true -UserMayChangePassword $false
    	
     #Create Install user
-	$user2 = "Install"
-	$fname2 = "Install"
-    $password2 = ConvertTo-SecureString "CloudIT1!" -AsPlainText -Force
+	$user2 = "name"
+	$fname2 = "fname"
+    $password2 = ConvertTo-SecureString "password" -AsPlainText -Force
     New-LocalUser $user2 -Password $password2 -FullName $fname2 -Description "the second admin user"
     Set-LocalUser $user2 -AccountNeverExpires -PasswordNeverExpires $true -UserMayChangePassword $false
 	
